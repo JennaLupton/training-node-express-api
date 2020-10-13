967c37f908ca6269f77108b5e416c0b62b85171d
Jenna Lupton <jenna.lupton@civica.co.uk>
2020-10-13 11:04:58 +0100

    Add new Recipe Steps tests and mock clearance

--- src/controllers/recipe-step.controller.test.js
+++ src/controllers/recipe-step.controller.test.js
@@ -23,9 +23,13 @@ getRecipeSteps.mockReturnValue(
   ])
 );
 deleteRecipeStep.mockReturnValue(Promise.resolve({ changes: 1 }));
-updateRecipeStep.mockReturnValue(Promise.resolve({ changes: 2 }));
-createRecipeStep.mockReturnValue(Promise.resolve({ changes: 4 }));
+updateRecipeStep.mockReturnValue(Promise.resolve({ changes: 1 }));
+createRecipeStep.mockReturnValue(Promise.resolve({ changes: 1 }));
 const { updateRecipeSteps } = require('./recipe-step.controller');
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
 
 describe('update recipe steps collection', () => {
   test('The correct number of changes is returned', async () => {
@@ -43,6 +47,82 @@ describe('update recipe steps collection', () => {
         step_text: 'new step two',
       },
     ]);
-    expect(result.changes).toEqual(7);
+    expect(result.changes).toEqual(3);
+    expect(getRecipeSteps.mock.calls.length).toEqual(1);
+    expect(updateRecipeStep.mock.calls.length).toEqual(1);
+    expect(deleteRecipeStep.mock.calls.length).toEqual(1);
+    expect(createRecipeStep.mock.calls.length).toEqual(1);
+  });
+});
+
+describe('update both recipe steps collection', () => {
+  test('The correct number of changes is returned', async () => {
+    const result = await updateRecipeSteps(1, [
+      {
+        recipe_step_id: 11,
+        recipe_id: 1,
+        step_number: 1,
+        step_text: 'step one',
+      },
+      {
+        recipe_step_id: 12,
+        recipe_id: 1,
+        step_number: 2,
+        step_text: 'step two',
+      },
+    ]);
+    expect(result.changes).toEqual(2);
+    expect(getRecipeSteps.mock.calls.length).toEqual(1);
+    expect(updateRecipeStep.mock.calls.length).toEqual(2);
+    expect(deleteRecipeStep.mock.calls.length).toEqual(0);
+    expect(createRecipeStep.mock.calls.length).toEqual(0);
+  });
+});
+
+describe('delete both recipe steps collection', () => {
+  test('The correct number of changes is returned', async () => {
+    const result = await updateRecipeSteps(1, [
+      {
+        recipe_step_id: null,
+        recipe_id: null,
+        step_number: null,
+        step_text: null,
+      },
+      {
+        recipe_step_id: null,
+        recipe_id: null,
+        step_number: null,
+        step_text: null,
+      },
+    ]);
+    expect(result.changes).toEqual(4);
+    expect(getRecipeSteps.mock.calls.length).toEqual(1);
+    expect(updateRecipeStep.mock.calls.length).toEqual(0);
+    expect(deleteRecipeStep.mock.calls.length).toEqual(2);
+    expect(createRecipeStep.mock.calls.length).toEqual(2);
+  });
+});
+
+describe('create both recipe steps collection', () => {
+  test('The correct number of changes is returned', async () => {
+    const result = await updateRecipeSteps(1, [
+      {
+        recipe_step_id: null,
+        recipe_id: 1,
+        step_number: 1,
+        step_text: 'new step one',
+      },
+      {
+        recipe_step_id: null,
+        recipe_id: 1,
+        step_number: 2,
+        step_text: 'new step two',
+      },
+    ]);
+    expect(result.changes).toEqual(4);
+    expect(getRecipeSteps.mock.calls.length).toEqual(1);
+    expect(updateRecipeStep.mock.calls.length).toEqual(0);
+    expect(deleteRecipeStep.mock.calls.length).toEqual(2);
+    expect(createRecipeStep.mock.calls.length).toEqual(2);
   });
 });
